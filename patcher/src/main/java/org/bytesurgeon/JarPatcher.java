36b531d766cf5d542f10bfc292d470655e1042e6
amrxtgh <ghimireamrit619@gmail.com>
2025-12-13 12:08:34 +0545

    feat: Jar In -> Jar Out only .class file is patched

--- patcher/src/main/java/org/bytesurgeon/JarPatcher.java
+++ patcher/src/main/java/org/bytesurgeon/JarPatcher.java
@@ -1,7 +1,36 @@
 package org.bytesurgeon;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.jar.JarOutputStream;
+
+import javax.management.RuntimeErrorException;
+
 final class JarPatcher {
     static void patch(Path input, Path output) throws Exception {
-        
+       try (JarFile jar = new JarFile(input.toFile());
+       JarOutputStream jos = new JarOutputStream(Files.newOutputStream(output))) {
+
+        jar.stream().forEach(entry -> {
+            try (InputStream is = jar.getInputStream(entry)) {
+                JarEntry outEntry = new JarEntry(entry.getName());
+                jos.putNextEntry(outEntry);
+
+                if (entry.getName().endsWith(".class")) {
+                   byte[] patched = ClassPatcher.patch(is);
+                   jos.write(patched);
+                } else {
+                    is.transferTo(jos);
+                }
+
+                jos.closeEntry();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+       } 
     }
 }
