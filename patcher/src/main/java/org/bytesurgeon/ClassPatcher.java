f4d162a559becc226cf1c2cd149cc44dc53fc8f0
amrxtgh <ghimireamrit619@gmail.com>
2025-12-16 22:17:05 +0545

    update: ClassPatcher.java

--- patcher/src/main/java/org/bytesurgeon/ClassPatcher.java
+++ patcher/src/main/java/org/bytesurgeon/ClassPatcher.java
@@ -1,1 +1,33 @@
 package org.bytesurgeon;
+
+import java.io.InputStream;
+
+import org.objectweb.asm.*;
+
+final class ClassPatcher {
+    static byte[] patch(InputStream in) throws Exception {
+        ClassReader cr = new ClassReader(in);
+
+        ClassWriter cw = new ClassWriter(
+            ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS
+        );
+        ClassVisitor cv = new ClassVisitor(ASM9, cw) {
+
+            private String className;
+            @Override
+            public void visit(
+                int version,
+                int access,
+                String name,
+                String signature,
+                String superName,
+                String[] interfaces) {
+                    this.className = name.replace("/", ".");
+                    super.visit(version, access, name, signature, interfaces);
+                }
+            }
+        }
+
+        return cw.toByteArray();
+    }
+}
